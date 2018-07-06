bbbafa17513b412bd00eff692fd608762f9876ce
John Duncan <jlduncan3@uh.edu>
2018-07-06 21:35:56 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,39 @@
+var number = 10
+
+function add5() {
+  number += 5
+
+}
+
+function divideBy3()
+{
+  number /= 3
+}
+
+divideby3()
+
+console.log(number)
+
+add5()
+
+console.log(number)
+
+number = 10
+
+add5()
+
+console.log(number)
+
+divideby3()
+
+console.log(number)
+
+function inc(n)
+{
+  n++
+}
+
+function dec(n)
+{
+  n--
+}
